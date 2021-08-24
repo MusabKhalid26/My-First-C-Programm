c95f1be7a1c9018ef5ad3ae32e97bbc307e11c28
Syed Faraz Khalid <36688688+Farazzaidi22@users.noreply.github.com>
2021-08-24 23:34:31 +0500

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//    float var1 = 5, var2 = 3;
+//    float var3, var4;
+//    printf("Enter your first number: ");
+//    scanf("%f", &var3);
+//    printf("Enter your second number: ");
+//    scanf("%f", &var4);
+//    float sum;
+//    sum = var3 + var4;
+//    printf("The answer is %f",sum);
+
+
+}
